3751165ee36f74d43b8eb64d98dcd3c436c54f7a
mnerisilva <marceloneri@gmail.com>
2021-06-15 16:53:59 -0300

    update

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const red_car = document.querySelector("svg #red-car");
 const yellow_car = document.querySelector("svg #yelloe-car");
 //car.style.display = "none";
@@ -26,32 +25,3 @@ gsap.to("#tax-car",{
     repeat: -1,
     repeatDelay: 2
 }); 
-=======
-const red_car = document.querySelector("svg #red-car");
-const yellow_car = document.querySelector("svg #yelloe-car");
-//car.style.display = "none";
-
-//car.style.transform = "translate("+635+"px, "+288+"px)";
-
-//transform: "translate("+635+"px, "+288+px")";
-
-gsap.to("#yellow-car",{
-    delay: 1.5,
-    duration: 7,
-    x: 1175,
-    y: -700,
-    ease: "none",
-    repeat: -1,
-    repeatDelay: 2
-}); 
-
-
-gsap.to("#tax-car",{
-    duration: 7,
-    x: 1175,
-    y: 600,
-    ease: "none",
-    repeat: -1,
-    repeatDelay: 2
-}); 
->>>>>>> c087979cb498976a351719b8f5af8790137ac305
